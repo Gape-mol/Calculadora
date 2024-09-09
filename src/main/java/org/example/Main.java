b2b26987cb0a45a55da6d8bc78d09df13df80377
Francisco <f.lizama05@ufromail.cl>
2024-09-09 16:19:23 -0300

    Iniciando funcion para resolver sistemas de ecuaciones

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+// Ax + By = C
+// Dx + Ey = F
+
+    public static String resolverSistemaEcuaciones(double numeroA, double numeroB, double numeroC,
+                                                   double numeroD, double numeroE, double numeroF) {
+        double [][] sistemaEcuaciones = new double[2][3];
+
+        sistemaEcuaciones[0][0] = numeroA;
+        sistemaEcuaciones[0][1] = numeroB;
+        sistemaEcuaciones[0][2] = numeroC;
+        sistemaEcuaciones[1][0] = numeroD;
+        sistemaEcuaciones[1][1] = numeroE;
+        sistemaEcuaciones[1][2] = numeroF;
+
+
     }
 }
