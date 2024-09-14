bab4b149e744a3a5b459c419f1fec842545bc8b1
BenjaG123 <b.garces01@ufromail.cl>
2024-09-13 23:04:01 -0300

    Se actualiza codigo test para resolver ecuaciones cuadraticas.

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,7 +1,48 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
+    @Test
+    public void testSolucionUnica() {
+        assertEquals(-1,Main.calcularSolucionUnica(1,2));
+    }
+
+    @Test
+    public void testSolucionUnica2() {
+        assertEquals(-1,Main.calcularSolucionUnica(2,4));
+    }
+
+    @Test
+    public void testCalcularDiscriminante() {
+        assertEquals(1, Main.calcularDiscriminante(2,9,10));
+    }
+
+    @Test
+    public void testCalcularDiscriminante2() {
+        assertEquals(16, Main.calcularDiscriminante(1,8,12));
+    }
+
+    @Test
+    public void testPrimeraSolucion() {
+        assertEquals(-2,Main.calcularPrimeraSolucion(2,9,1));
+    }
+
+    @Test
+    public void testPrimeraSolucion2() {
+        assertEquals(-2,Main.calcularPrimeraSolucion(1,8,16));
+    }
+
+    @Test
+    public void testSegundaSolucion() {
+        assertEquals(-2.5,Main.calcularSegundaSolucion(2,9,1));
+    }
+
+    @Test
+    public void testSegundaSolucion2() {
+        assertEquals(-6,Main.calcularSegundaSolucion(1,8,16));
+    }
 }
