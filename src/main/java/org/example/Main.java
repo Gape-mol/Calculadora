5857585cc48bf4033817d738098af724a0b800ae
Francisco <f.lizama05@ufromail.cl>
2024-09-13 21:41:34 -0300

    Funciones numerMenor y numeroMayor testeadas y actualizadas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,36 +7,28 @@ public class Main {
 //Funcion para determinar el mayor entre 2 numeros
 
     public static double numeroMayor(double numero1, double numero2) {
-        double mayor = 0;
         if (numero1 > numero2) {
-            mayor = numero1;
-            return mayor;
+            return numero1;
         } else if (numero2 > numero1) {
-            mayor = numero2;
-            return mayor;
+            return numero2;
         } else if (numero1 == numero2) {
-            mayor = numero2;
-            return mayor;
+            return numero1;
         } else {
-            return mayor;
+            return 0;
         }
     }
 
 //Funcion para encontrar el menor de 2 numeros
 
     public static double numeroMenor(double numero1, double numero2) {
-        double menor = 0;
         if (numero1 < numero2) {
-            menor = numero1;
-            return menor;
+            return numero1;
         } else if (numero2 < numero1) {
-            menor = numero2;
-            return menor;
+            return numero2;
         } else if (numero1 == numero2) {
-            menor = numero2;
-            return menor;
+            return numero1;
         } else {
-            return menor;
+            return 0;
         }
     }
 
