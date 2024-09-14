237772f43543f2720a868d536f24f30d2d0a3ca2
Francisco <f.lizama05@ufromail.cl>
2024-09-13 21:43:53 -0300

    Funciones numerMenor y numeroMayor testeadas y actualizadas

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+
+    @Test
+    void numeroMayor() {
+        assertEquals(3.14, Main.numeroMayor(3.14, 1));
+
+        assertEquals(-1 , Main.numeroMayor(-4, -1));
+
+        assertEquals(10, Main.numeroMayor(10, 10));
+    }
+
+    @Test
+    void numeroMenor() {
+        assertEquals(1, Main.numeroMenor(3.14, 1));
+
+        assertEquals(-4 , Main.numeroMenor(-4, -1));
+
+        assertEquals(10, Main.numeroMenor(10, 10));
+    }
 
 }
