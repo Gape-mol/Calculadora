099dc7d8ccd209e9bb904a792eeb69ce3069daec
Gape-mol <g.perez11@ufromail.cl>
2024-09-15 16:57:53 -0300

    Terminado el test y añadidos try/catchs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -43,33 +43,87 @@ public class Main {
     // Perimetro: (base x 2) + (altura x 2)
     // Area: base x altura
     public static float perimetroRectangulo(float base, float altura) {
-        return (base * 2) + (altura * 2);
+        try {
+            if (base <= 0 || altura <= 0) {
+                throw new IllegalArgumentException("La base y la altura no pueden ser menores o iguales a 0");
+            } else {
+                return (base * 2) + (altura * 2);
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
     }
 
     public static float areaRectangulo(float base, float altura) {
-        return base * altura;
+        try {
+            if (base <= 0 || altura <= 0) {
+                throw new IllegalArgumentException("La base y la altura no pueden ser menores o iguales a 0");
+            } else {
+                return base * altura;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
     }
 
     //Círculo:
     // Perimetro: 2 x pi x radio
     // Area: pi x radio x radio
     public static double perimetroCirculo(float radio) {
-        return 2 * Math.PI * radio;
+        try {
+            if (radio <= 0) {
+                throw new IllegalArgumentException("El radio no puede ser menor o igual a 0");
+            } else {
+                return 2 * Math.PI * radio;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
     }
 
     public static double areaCirculo(float radio) {
-        return Math.PI * radio * radio;
+        try {
+            if (radio <= 0) {
+                throw new IllegalArgumentException("El radio no puede ser menor o igual a 0");
+            } else {
+                return Math.PI * radio * radio;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
     }
 
     //Esfera:
     // Area: 4 x pi x radio x radio
     // Volumen: (4/3) x pi x radio x radio x radio
     public static double areaEsfera(float radio) {
-        return 4 * areaCirculo(radio);
+        try {
+            if (radio <= 0) {
+                throw new IllegalArgumentException("El radio no puede ser menor o igual a 0");
+            } else {
+                return 4 * Math.PI * radio * radio;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
     }
 
     public static double volumenEsfera(float radio) {
-        return ((double) 4 / 3) * Math.PI * radio * radio * radio;
+        try {
+            if (radio <= 0) {
+                throw new IllegalArgumentException("El radio no puede ser menor o igual a 0");
+            } else {
+                return ((double) 4 / 3) * Math.PI * radio * radio * radio;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
     }
 
     //Cubo:
@@ -77,27 +131,72 @@ public class Main {
     // Volumen: lado x lado x lado
     // Area: 6 x lado x lado
     public static float perimetroCubo(float lado) {
-        return 3 * perimetroCuadrado(lado);
+        try {
+            if (lado <= 0) {
+                throw new IllegalArgumentException("El lado no puede ser menor o igual a 0");
+            } else {
+                return 12 * lado;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
     }
 
     public static float areaCubo(float lado) {
-        return 6 * areaCuadrado(lado);
+        try {
+            if (lado <= 0) {
+                throw new IllegalArgumentException("El lado no puede ser menor o igual a 0");
+            } else {
+                return 6 * lado * lado;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
     }
 
     public static float volumenCubo(float lado) {
-        return areaCuadrado(lado) * lado;
+        try {
+            if (lado <= 0) {
+                throw new IllegalArgumentException("El lado no puede ser menor o igual a 0");
+            } else {
+                return lado * lado * lado;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
     }
 
     //Cono:
     // Area: pi x radio x radio + raiz cuadrada de (radio x radio + altura x altura)
     // Volumen: pi x radio x radio x altura)/3
     public static double areaCono(float radio, float altura) {
-        double areaCirculo = areaCirculo(radio);
-        double areaTriangulo = Math.PI * radio * (Math.sqrt((radio * radio) + (altura * altura)));
-        return areaCirculo + areaTriangulo;
+        try {
+            if (radio <= 0 || altura <= 0) {
+                throw new IllegalArgumentException("El radio y la altura no pueden ser menores o iguales a 0");
+            } else {
+                double areaCirculo = areaCirculo(radio);
+                double areaTriangulo = Math.PI * radio * (Math.sqrt((radio * radio) + (altura * altura)));
+                return areaCirculo + areaTriangulo;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
     }
 
     public static double volumenCono(float radio, float altura) {
-        return (Math.PI * (radio * radio) * altura) / 3;
+        try {
+            if (radio <= 0 || altura <= 0) {
+                throw new IllegalArgumentException("El radio y la altura no pueden ser menores o iguales a 0");
+            } else {
+                return ((Math.PI * (radio * radio) * altura) / 3);
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -5,28 +5,176 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-    Main main = new Main();
 
     @Test
     void perimetroCuadrado() {
         //Lado negativo
-        assertEquals(0, main.perimetroCuadrado(-1));
+        assertEquals(0, Main.perimetroCuadrado(-1));
         //Lado 0
-        assertEquals(0, main.perimetroCuadrado(0));
+        assertEquals(0, Main.perimetroCuadrado(0));
         //Lado int
-        assertEquals(8, main.perimetroCuadrado(2));
+        assertEquals(8, Main.perimetroCuadrado(2));
         //Lado float
-        assertEquals(12.4, main.perimetroCuadrado(3.1F) , 0.1);
+        assertEquals(12.4, Main.perimetroCuadrado(3.1F) , 0.1);
     }
     @Test
     void areaCuadrado() {
         //Lado negativo
-        assertEquals(0, main.areaCuadrado(-1));
+        assertEquals(0, Main.areaCuadrado(-1));
         //Lado 0
-        assertEquals(0, main.areaCuadrado(0));
+        assertEquals(0, Main.areaCuadrado(0));
         //Lado int
-        assertEquals(4, main.areaCuadrado(2));
+        assertEquals(4, Main.areaCuadrado(2));
         //Lado float
-        assertEquals(9.61, main.areaCuadrado(3.1F) , 0.1);
+        assertEquals(9.61, Main.areaCuadrado(3.1F) , 0.1);
     }
+
+    @Test
+    void perimetroRectangulo() {
+        //Base negativa
+        assertEquals(0, Main.perimetroRectangulo(-1, 2));
+        //Altura negativa
+        assertEquals(0, Main.perimetroRectangulo(2, -1));
+        //Base 0
+        assertEquals(0, Main.perimetroRectangulo(0, 2));
+        //Altura 0
+        assertEquals(0, Main.perimetroRectangulo(2, 0));
+        //Ambos int
+        assertEquals(10, Main.perimetroRectangulo(3, 2));
+        //Ambos float
+        assertEquals(10.2, Main.perimetroRectangulo(3.1F, 2.0F), 0.1);
+    }
+
+    @Test
+    void areaRectangulo() {
+        //Base negativa
+        assertEquals(0, Main.areaRectangulo(-1, 2));
+        //Altura negativa
+        assertEquals(0, Main.areaRectangulo(2, -1));
+        //Base 0
+        assertEquals(0, Main.areaRectangulo(0, 2));
+        //Altura 0
+        assertEquals(0, Main.areaRectangulo(2, 0));
+        //Ambos int
+        assertEquals(6, Main.areaRectangulo(3, 2));
+        //Ambos float
+        assertEquals(6.2, Main.areaRectangulo(3.1F, 2.0F), 0.1);
+    }
+
+    @Test
+    void perimetroCirculo() {
+        //Radio negativo
+        assertEquals(0, Main.perimetroCirculo(-1));
+        //Radio 0
+        assertEquals(0, Main.perimetroCirculo(0));
+        //Radio int
+        assertEquals(6.2, Main.perimetroCirculo(1), 0.1);
+        //Radio float
+        assertEquals(9.4, Main.perimetroCirculo(1.5F), 0.1);
+    }
+
+    @Test
+    void areaCirculo() {
+        //Radio negativo
+        assertEquals(0, Main.areaCirculo(-1));
+        //Radio 0
+        assertEquals(0, Main.areaCirculo(0));
+        //Radio int
+        assertEquals(3.1, Main.areaCirculo(1), 0.1);
+        //Radio float
+        assertEquals(7.1, Main.areaCirculo(1.5F), 0.1);
+    }
+
+    @Test
+    void areaEsfera() {
+        //Radio negativo
+        assertEquals(0, Main.areaEsfera(-1));
+        //Radio 0
+        assertEquals(0, Main.areaEsfera(0));
+        //Radio int
+        assertEquals(12.5, Main.areaEsfera(1), 0.1);
+        //Radio float
+        assertEquals(28.2, Main.areaEsfera(1.5F), 0.1);
+    }
+
+    @Test
+    void volumenEsfera() {
+        //Radio negativo
+        assertEquals(0, Main.volumenEsfera(-1));
+        //Radio 0
+        assertEquals(0, Main.volumenEsfera(0));
+        //Radio int
+        assertEquals(4.2, Main.volumenEsfera(1), 0.1);
+        //Radio float
+        assertEquals(14.1, Main.volumenEsfera(1.5F), 0.1);
+    }
+
+    @Test
+    void perimetroCubo() {
+        //Lado negativo
+        assertEquals(0, Main.perimetroCubo(-1));
+        //Lado 0
+        assertEquals(0, Main.perimetroCubo(0));
+        //Lado int
+        assertEquals(24, Main.perimetroCubo(2));
+        //Lado float
+        assertEquals(18.6, Main.perimetroCubo(3.1F), 0.1);
+    }
+
+    @Test
+    void areaCubo() {
+        //Lado negativo
+        assertEquals(0, Main.areaCubo(-1));
+        //Lado 0
+        assertEquals(0, Main.areaCubo(0));
+        //Lado int
+        assertEquals(24, Main.areaCubo(2));
+        //Lado float
+        assertEquals(54.6, Main.areaCubo(3.1F), 0.5);
+    }
+
+    @Test
+    void volumenCubo() {
+        //Lado negativo
+        assertEquals(0, Main.volumenCubo(-1));
+        //Lado 0
+        assertEquals(0, Main.volumenCubo(0));
+        //Lado int
+        assertEquals(8, Main.volumenCubo(2));
+        //Lado float
+        assertEquals(27.1, Main.volumenCubo(3.1F), 0.1);
+    }
+
+    @Test
+    void areaCono() {
+        //Radio negativo
+        assertEquals(0, Main.areaCono(-1, 2));
+        //Altura negativa
+        assertEquals(0, Main.areaCono(2, -1));
+        //Radio 0
+        assertEquals(0, Main.areaCono(0, 2));
+        //Altura 0
+        assertEquals(0, Main.areaCono(2, 0));
+        //Ambos int
+        assertEquals(12.6, Main.areaCono(1, 3), 0.5);
+        //Ambos float
+        assertEquals(19.6, Main.areaCono(1.5F, 3.1F), 0.5);
+    }
+
+    @Test
+    void volumenCono() {
+        //Radio negativo
+        assertEquals(0, Main.volumenCono(-1, 2));
+        //Altura negativa
+        assertEquals(0, Main.volumenCono(2, -1));
+        //Radio 0
+        assertEquals(0, Main.volumenCono(0, 2));
+        //Altura 0
+        assertEquals(0, Main.volumenCono(2, 0));
+        //Ambos int
+        assertEquals(1, Main.volumenCono(1, 3), 0.1);
+        //Ambos float
+        assertEquals(4.7, Main.volumenCono(1.5F, 3.1F), 0.1);
+    }
+
 }
